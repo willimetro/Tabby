189dec5539fb70da15f40fc120586399d4e6eb97
willimetro <wilfredo.ugalde@gmail.com>
2018-02-21 02:07:06 -0300

    Commit Inicial

---
+++ app/src/main/java/com/udemy/wilfredo/tabby/activities/TabbyActivity.java
@@ -0,0 +1,15 @@
+package com.udemy.wilfredo.tabby.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.udemy.wilfredo.tabby.R;
+
+public class TabbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabby);
+    }
+}
